8cfc6ed2cf4d664929f3c1a93a7af4e4c82e88f8
Rajeshkumar671 <rajeshkumar@rorwebcoders.com>
2024-07-03 01:02:21 +0530

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
-  config.hosts << "athirstamfinance.onrender.com"
+  Rails.application.config.hosts << "athirstamfinance.onrender.com"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
