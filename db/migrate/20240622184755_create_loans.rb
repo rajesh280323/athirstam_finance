a4a1cce6bfda1e1dd256231da77a98c325e8d85c
Rajeshkumar671 <rajeshkumar@rorwebcoders.com>
2024-07-10 01:21:32 +0530

    change

--- db/migrate/20240622184755_create_loans.rb
+++ db/migrate/20240622184755_create_loans.rb
@@ -10,7 +10,7 @@ class CreateLoans < ActiveRecord::Migration[7.1]
       t.date :weekly_collection_date
       t.date :first_ewi_date
       t.date :loan_closing_date
-      t.boolean :status, default: true
+      t.integer :status, default: '0'
 
       t.timestamps
       t.references :leader, null: true, foreign_key: true
